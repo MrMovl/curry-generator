c83c0765ebf03bae8b3296896578821cc44ac311
Tomke Reibisch <treibisch@gmail.com>
2016-01-03 15:37:08 +0100

    I now can select more then one spice, but the randomicity seems to be off, I think I need to generate new seeds during my recursion... could be tough

--- curry.elm
+++ curry.elm
@@ -4,25 +4,27 @@ import Html.Events exposing (onClick)
 import StartApp.Simple as StartApp
 import Random exposing (initialSeed, int, generate, Seed)
 import Time exposing (every)
+import List exposing (foldl)
+import String exposing (length)
 
 main =
   StartApp.start { model = model, view = view, update = update }
 
 base = [ "onions", "coconut milk", "tomatoes", "joghurt and creme" ]
-spices = [ "pepper", "garam masala" ]
-mainIngredient = [ "chicken", "lamb", "cauliflower" ]
+spices = [ "Pfeffer", "Garam Masala", "Kumin", "Kardamom", "Senfsamen", "Zimt", "Gewürznelken", "Chilli", "Ingwer" ]
+mainIngredient = [ "chicken", "lamb", "cauliflower", "Aubergine" ]
 
 
 type alias Model = 
   { base : String
-  , spices : String
+  , spices : List String
   , mainIngredient : String
   , seed : Seed
   }
 
 model = 
   { base = ""
-  , spices = ""
+  , spices = []
   , mainIngredient = ""
   , seed = initialSeed 42
   }
@@ -31,8 +33,8 @@ view address model =
   div []
     [ button [ onClick address Generate ] [ text "Generate recipe" ]
     , div [] [ text ("This will be you watery base: " ++ model.base) ]
-    , div [] [ text ("You can use these spices: " ++ model.spices) ]
-    , div [] [ text ("And add this as your main ingredient: " ++ model.mainIngredient)]
+    , div [] [ text ("You can use these spices: " ++ foldl (++) "" model.spices) ]
+    , div [] [ text ("And add this as your main ingredient: " ++ model.mainIngredient) ]
     ]
 
 
@@ -46,20 +48,35 @@ update action model =
 createRandomRecipe : Model -> Model
 createRandomRecipe model = 
   { base = pickIngredient base model.seed
-  , spices = pickIngredient spices model.seed
+  , spices = pickStuff spices model.seed 3
   , mainIngredient = pickIngredient mainIngredient model.seed
-  , seed = getNewSeed model.seed
+  , seed = getNewSeed model
   }
 
-getNewSeed : Seed -> Seed
-getNewSeed oldSeed =
-  let gen = Random.int 0 10
-      (i, seed) = Random.generate gen oldSeed
-  in seed
+-- uses semi-random data to generate a new seed for the next recipe
+getNewSeed : Model -> Seed
+getNewSeed model =
+  let 
+    gen = Random.int (length model.base) (length model.mainIngredient)
+    (i, seed) = Random.generate gen model.seed
+  in
+    seed
+
+pickStuff : List String -> Seed -> Int -> List String
+pickStuff list seed count =
+  if 
+    count == 1
+  then
+    pickIngredient list seed :: []
+  else
+    pickIngredient list seed :: pickStuff list seed (count - 1)
+
 
 pickIngredient : List String -> Seed -> String
-pickIngredient list oldSeed = 
-  let randomIndexGenerator = Random.int 0 (List.length list - 1)
-      (i, seed) = Random.generate randomIndexGenerator oldSeed
-      elem = List.drop i list |> List.head
-  in Maybe.withDefault "" elem
+pickIngredient list seed = 
+  let 
+    randomIndexGenerator = Random.int 0 (List.length list - 1)
+    (i, s) = Random.generate randomIndexGenerator seed
+    elem = List.drop i list |> List.head
+  in
+    (Maybe.withDefault "" elem)
